810b1975d6dd6ffc2ffc8fe573c7a34e04bc3624
bascome <eva.bascompte.moragas@vumc.org>
2019-06-17 09:45:59 -0500

    Added extra check in cancel button

--- cancelImport.php
+++ cancelImport.php
@@ -7,11 +7,14 @@ $edoc_list = $module->getProjectSetting('edoc');
 
 $edoc = array_pop(array_reverse($edoc_list));
 if (($key = array_search($edoc, $edoc_list)) !== false) {
-    $import_cancel[$key] = true;
+    $import = $module->getProjectSetting('import', $project_id);
+    if($import[$key] == false){
+        $import_cancel[$key] = true;
 
-    $module->setProjectSetting('import-cancel', $import_cancel,$project_id);
+        $module->setProjectSetting('import-cancel', $import_cancel,$project_id);
 
-    \REDCap::logEvent("<i>Big Data Import</i> process <b>cancelled</b>","user = ".USERID."\nFile = '".$module->getDocName($edoc)."'",null,null,null,$project_id);
+        \REDCap::logEvent("<i>Big Data Import</i> process <b>cancelled</b>","user = ".USERID."\nFile = '".$module->getDocName($edoc)."'",null,null,null,$project_id);
+    }
 }
 
 echo json_encode(array(
