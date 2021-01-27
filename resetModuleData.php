5f65f8d31c75854b1d11b39dfa8d71ed43055a9d
Ekin Tertemiz <ekin.tertemiz@swisstph.ch>
2021-01-27 10:13:12 +0100

    Refactoring

--- resetModuleData.php
+++ resetModuleData.php
@@ -5,19 +5,19 @@
 
 $settings = $module->getProjectSettings();
 
+# Ignore settings that are not relevant
 unset($settings["enabled"]);
 unset($settings["version"]);
 unset($settings["import-email"]);
 unset($settings["import-from"]);
 
+# Remove project setting with specified key from current project
 foreach ($settings as $key => $value) {
-    // Removes project setting with specified key from current project
     $module->removeProjectSetting($key);
 }
 
+# Send response and log 
 $icon = '<span class="fa-stack fa-2x"><i class="fas fa-circle fa-stack-2x"></i><i class="fas fa-redo fa-stack-1x fa-inverse"></i></span>';
-
-
 $logtext = "The module has been reset. {$icon}</div>";
 
 $module->log($logtext);
@@ -26,7 +26,5 @@ echo json_encode(array(
     'status' => 'success',
     'message' => $logtext
 ));
-
-
 
 ?>
